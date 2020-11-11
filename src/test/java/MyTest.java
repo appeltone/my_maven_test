73cfb3ab2aed6646e4ddd9c4d6a9e844f097954e
appeltone <72234136+appeltone@users.noreply.github.com>
2020-11-11 21:04:02 +0200

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public void test_001(){
         System.out.println("Printed from my test class");
         System.out.println("new print after initial upload to git");
+        //pull test 1 2 3
     }
 }
